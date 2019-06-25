a310fd886cd78ceddeee6ca474dfb77678b64184
Liew Manchoi <liewmanchoi@gmail.com>
2019-06-25 09:38:54 +0800

    feat(registry.zookeeper): NodeUtils
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/pigeon/rpc/registry/zookeeper/utils/NodeUtils.java
@@ -0,0 +1,54 @@
+package com.liewmanchoi.pigeon.rpc.registry.zookeeper.utils;
+
+import com.liewmanchoi.pigeon.rpc.protocol.InvokerInfo;
+import com.liewmanchoi.pigeon.rpc.protocol.ProviderInfo;
+import com.liewmanchoi.pigeon.rpc.utils.IpAddressUtils;
+
+
+/**
+ * @author wangsheng
+ * @date 2019/6/24
+ */
+public class NodeUtils {
+    private static final String PROVIDER_TYPE = "providers";
+    private static final String INVOKER_TYPE = "invokers";
+
+    public static String getCommonPrefix(ProviderInfo providerInfo) {
+        return providerInfo.getAppKey() + "/" +
+                providerInfo.getGroupName() + "/" +
+                providerInfo.getServiceInterface().getName();
+    }
+
+    public static String getCommonPrefix(InvokerInfo invokerInfo) {
+        return invokerInfo.getRemoteAppKey() + "/" +
+                invokerInfo.getGroupName() + "/" +
+                invokerInfo.getServiceInterface().getName();
+    }
+
+    public static String getProviderParentNode(ProviderInfo providerInfo) {
+         return providerInfo.getAppKey() + "/" +
+                providerInfo.getGroupName() + "/" +
+                providerInfo.getServiceInterface().getName() + "/" +
+                PROVIDER_TYPE;
+    }
+
+    public static String getProviderNode(ProviderInfo providerInfo) {
+        return getProviderParentNode(providerInfo) + "/" +
+                IpAddressUtils.getIpAddress() + ":" +
+                providerInfo.getServerPort();
+
+    }
+
+    public static String getInvokerParentNode(InvokerInfo invokerInfo) {
+        return invokerInfo.getRemoteAppKey() + "/" +
+                invokerInfo.getGroupName() + "/" +
+                invokerInfo.getServiceInterface().getName() + "/" +
+                INVOKER_TYPE;
+
+    }
+
+    public static String getInvokerNode(InvokerInfo invokerInfo) {
+        return getInvokerParentNode(invokerInfo) + "/" +
+                IpAddressUtils.getIpAddress();
+    }
+}
