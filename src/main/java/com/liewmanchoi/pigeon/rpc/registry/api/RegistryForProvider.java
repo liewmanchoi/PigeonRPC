2428e361de352cf07961bf01fbfd7c0082ee2b88
Liew Manchoi <liewmanchoi@gmail.com>
2019-06-25 09:38:12 +0800

    refactor(registry.api): 修改接口名
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- src/main/java/com/liewmanchoi/pigeon/rpc/registry/api/RegistryForProvider.java
+++ src/main/java/com/liewmanchoi/pigeon/rpc/registry/api/RegistryForProvider.java
@@ -2,9 +2,6 @@ package com.liewmanchoi.pigeon.rpc.registry.api;
 
 import com.liewmanchoi.pigeon.rpc.protocol.ProviderInfo;
 
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author wangsheng
  * @date 2019/6/22
@@ -13,13 +10,7 @@ public interface RegistryForProvider {
     /**
      * 向注册中心注册服务提供者信息
      * @param providerInfo 服务提供者信息
+     * @exception Exception 异常
      */
-    void registerProvider(final ProviderInfo providerInfo);
-    /**
-     * 获取服务提供者信息
-     *
-     * @return java.util.Map<java.lang.String, java.util.List < com.liewmanchoi.pigeon.rpc.protocol.ProviderService>>
-     * @date 2019/6/22
-     */
-    Map<String, List<ProviderInfo>> getProviderInfoMap();
+    void registerProvider(final ProviderInfo providerInfo) throws Exception;
 }
