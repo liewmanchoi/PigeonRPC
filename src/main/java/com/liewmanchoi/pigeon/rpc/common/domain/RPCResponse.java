669fa69fb45fe5facb9fcf1a3a6a2c59bf540341
Liew Manchoi <liewmanchoi@gmail.com>
2019-06-26 17:41:06 +0800

    feat(common.domain): RPCRequest
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- src/main/java/com/liewmanchoi/pigeon/rpc/common/domain/RPCResponse.java
+++ src/main/java/com/liewmanchoi/pigeon/rpc/common/domain/RPCResponse.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
  * RPC调用响应
  *
  * 使用场景：
- * 1) provider调用完服务后创建该对象，经过序列化之后传递给invoker，然后被对象池回收
+ * 1) provider调用完服务后创建该对象，经过序列化之后传递给invoker，然后被对象池回收 <br>
  * 2) invoker收到响应，进行反序列化后创建该对象，返回给用户之前被对象池回收
  *
  * @author wangsheng
