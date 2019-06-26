073fc9a8105ba75142b14aec59e30a63a0d0fafc
Liew Manchoi <liewmanchoi@gmail.com>
2019-06-26 16:18:25 +0800

    feat(common.enumeration): ErrorEnum
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/pigeon/rpc/common/enumeration/ErrorEnum.java
@@ -0,0 +1,23 @@
+package com.liewmanchoi.pigeon.rpc.common.enumeration;
+
+import lombok.Getter;
+
+/**
+ * @author wangsheng
+ */
+public enum ErrorEnum {
+    /**
+     * 序列化故障
+     */
+    SERIALIZATION_ERROR("序列化故障"),
+    /**
+     * 自定义RPCException消息模板替换出错
+     */
+    TEMPLATE_REPLACEMENT_ERROR("自定义RPCException消息模板替换出错");
+
+    private @Getter String errorCode;
+
+    ErrorEnum(String errorCode) {
+        this.errorCode = errorCode;
+    }
+}
