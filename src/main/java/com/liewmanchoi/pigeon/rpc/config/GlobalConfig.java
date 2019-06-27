bbacdbf91004c2a431dd289cbb2e07d33acb1838
Liew Manchoi <liewmanchoi@gmail.com>
2019-06-27 21:30:19 +0800

    feat(transport.api.support): client端抽象基类
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/pigeon/rpc/config/GlobalConfig.java
@@ -0,0 +1,20 @@
+package com.liewmanchoi.pigeon.rpc.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 全局配置类。类中持有相关配置的实例
+ *
+ * @author wangsheng
+ * @date 2019/6/26
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GlobalConfig {
+    // TODO: GlobalConfig待完成
+}
---
+++ src/main/java/com/liewmanchoi/pigeon/rpc/config/ServiceConfig.java
@@ -0,0 +1,11 @@
+package com.liewmanchoi.pigeon.rpc.config;
+
+/**
+ * 服务配置类
+ *
+ * @author wangsheng
+ * @date 2019/6/27
+ */
+public class ServiceConfig<T> {
+    // TODO: 待实现ServiceConfig类
+}
