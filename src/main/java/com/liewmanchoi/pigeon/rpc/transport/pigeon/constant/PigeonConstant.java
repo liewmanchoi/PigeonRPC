f560bf6747450a5ac4e1f450a26836b3ea4a8f2d
Liew Manchoi <liewmanchoi@gmail.com>
2019-06-27 10:38:55 +0800

    feat(transport.pigeon.constant): 关于服务器断开重连的时间参数
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/pigeon/rpc/transport/pigeon/constant/PigeonConstant.java
@@ -0,0 +1,10 @@
+package com.liewmanchoi.pigeon.rpc.transport.pigeon.constant;
+
+/**
+ * @author wangsheng
+ * @date 2019/6/27
+ */
+public class PigeonConstant {
+    public static final int HEART_BEAT_TIME_OUT = 20;
+    public static final int HEART_BEAT_TIME_OUT_MAX_TIME = 3;
+}
