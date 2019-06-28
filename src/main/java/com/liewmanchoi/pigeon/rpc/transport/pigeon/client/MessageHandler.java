2d7bba38fc7e9c0ca8ced8c8aba99addc8f311de
Liew Manchoi <liewmanchoi@gmail.com>
2019-06-28 20:37:50 +0800

    feat(transport.pigeon.client): 自定义协议客户端
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/pigeon/rpc/transport/pigeon/client/MessageHandler.java
@@ -0,0 +1,39 @@
+package com.liewmanchoi.pigeon.rpc.transport.pigeon.client;
+
+import com.liewmanchoi.pigeon.rpc.common.domain.Message;
+import com.liewmanchoi.pigeon.rpc.transport.api.Client;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 消息处理器
+ *
+ * @author wangsheng
+ * @date 2019/6/28
+ */
+@Slf4j
+@AllArgsConstructor
+public class MessageHandler extends ChannelInboundHandlerAdapter {
+    private Client client;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        Message message = (Message) msg;
+        log.info("接收到服务器-{} 响应-{}", client.getServiceURL().getAddress(), message);
+
+        // 消息类型
+        byte type = message.getType();
+        // 约定：服务器不会PING客户端
+        if (type == Message.PONG) {
+            log.info("收到服务器PONG响应消息");
+        } else if (type == Message.RESPONSE) {
+            client.handleRPCResponse(message.getRpcResponse());
+        } else if (type == Message.REQUEST) {
+            client.handleCallbackRequest(message.getRpcRequest(), ctx);
+        }
+
+        super.channelRead(ctx, msg);
+    }
+}
---
+++ src/main/java/com/liewmanchoi/pigeon/rpc/transport/pigeon/client/PigeonClient.java
@@ -0,0 +1,55 @@
+package com.liewmanchoi.pigeon.rpc.transport.pigeon.client;
+
+import com.liewmanchoi.pigeon.rpc.transport.api.constant.FrameConstant;
+import com.liewmanchoi.pigeon.rpc.transport.api.converter.AbstractClientMessageConverter;
+import com.liewmanchoi.pigeon.rpc.transport.api.support.AbstractClient;
+import com.liewmanchoi.pigeon.rpc.transport.pigeon.codec.PigeonDecoder;
+import com.liewmanchoi.pigeon.rpc.transport.pigeon.codec.PigeonEncoder;
+import com.liewmanchoi.pigeon.rpc.transport.pigeon.constant.PigeonConstant;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.timeout.IdleStateHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author wangsheng
+ * @date 2019/6/28
+ */
+@Slf4j
+public class PigeonClient extends AbstractClient {
+    @Override
+    protected ChannelInitializer initPipeline() {
+        log.info("PigeonClient init pipeline...");
+        return new ChannelInitializer() {
+            @Override
+            protected void initChannel(Channel ch) throws Exception {
+                ch.pipeline()
+                // 出站编码器
+                .addLast("IdleStateHandler", new IdleStateHandler(0, PigeonConstant.HEART_BEAT_TIME_OUT, 0))
+                        // 添加length域解决半包/拆包问题
+                .addLast("LengthFieldPrepender", new LengthFieldPrepender(FrameConstant.LENGTH_FIELD_LENGTH,
+                        FrameConstant.LENGTH_ADJUSTMENT))
+                .addLast("PigeonEncoder", new PigeonEncoder(getGlobalConfig().getSerializer()))
+                // 入站解码器
+                .addLast("LengthFieldBasedFrameDecoder",
+                        new LengthFieldBasedFrameDecoder(FrameConstant.MAX_FRAME_LENGTH,
+                                FrameConstant.LENGTH_FIELD_OFFSET, FrameConstant.LENGTH_FIELD_LENGTH,
+                                FrameConstant.LENGTH_ADJUSTMENT, FrameConstant.INITIAL_BYTES_TO_STRIP))
+                        // ByteBuf -> Message
+                .addLast("PigeonDecoder", new PigeonDecoder(getGlobalConfig().getSerializer()))
+                // 心跳检测
+                .addLast("HearBeatHandler", new HeartBeatHandler(PigeonClient.this))
+                // 处理消息
+                .addLast("MessageHandler", new MessageHandler(PigeonClient.this));
+            }
+        };
+    }
+
+    @Override
+    protected AbstractClientMessageConverter initConverter() {
+        // 对于自定义Pigeon协议来说，实质上没有进行转换
+        return AbstractClientMessageConverter.DEFAULT_IMPL;
+    }
+}
