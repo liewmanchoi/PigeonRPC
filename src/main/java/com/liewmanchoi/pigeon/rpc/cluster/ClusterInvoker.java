4c1a7836b33c14105fa7c755200ff531b61a1c80
Liew Manchoi <liewmanchoi@gmail.com>
2019-07-04 12:03:59 +0800

    feat(cluster): 完成ClusterInvoker类未完成的功能
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- src/main/java/com/liewmanchoi/pigeon/rpc/cluster/ClusterInvoker.java
+++ src/main/java/com/liewmanchoi/pigeon/rpc/cluster/ClusterInvoker.java
@@ -13,13 +13,14 @@ import com.liewmanchoi.pigeon.rpc.config.ReferenceConfig;
 import com.liewmanchoi.pigeon.rpc.protocol.api.invoker.Invoker;
 import com.liewmanchoi.pigeon.rpc.protocol.api.protocol.Protocol;
 import com.liewmanchoi.pigeon.rpc.protocol.api.protocol.support.AbstractRemoteProtocol;
+import com.liewmanchoi.pigeon.rpc.registry.api.EventHandler;
+import com.liewmanchoi.pigeon.rpc.registry.api.ServiceRegistry;
 import com.liewmanchoi.pigeon.rpc.registry.api.ServiceURL;
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 对应于一个interface的所有服务器实现的抽象 <br>
@@ -30,139 +31,162 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @Slf4j
 public class ClusterInvoker<T> implements Invoker<T> {
-    private Class<T> interfaceClass;
-    private GlobalConfig globalConfig;
+  private Class<T> interfaceClass;
+  private GlobalConfig globalConfig;
 
-    /**
-     * ClusterInvoker中包含的invoker列表
-     * 键为invoker所在的地址
-     */
-    private Map<String, Invoker<T>> invokerMap = new ConcurrentHashMap<>();
+  /** ClusterInvoker中包含的invoker列表 键为invoker所在的地址 */
+  private Map<String, Invoker<T>> invokerMap = new ConcurrentHashMap<>();
 
-    public ClusterInvoker(Class<T> interfaceClass, GlobalConfig globalConfig) {
-        this.interfaceClass = interfaceClass;
-        this.globalConfig = globalConfig;
-        init();
+  public ClusterInvoker(Class<T> interfaceClass, GlobalConfig globalConfig) {
+    this.interfaceClass = interfaceClass;
+    this.globalConfig = globalConfig;
+    init();
+  }
+
+  private void init() {
+
+    if (globalConfig.getProtocol() instanceof AbstractRemoteProtocol) {
+      ServiceRegistry serviceRegistry = globalConfig.getServiceRegistry();
+      // 进行服务发现
+      serviceRegistry.discover(getInterfaceName(), new EventHandlerAdapter());
+    }
+  }
+
+  @Override
+  public RPCResponse invoke(RPCRequestWrapper rpcRequestWrapper) throws RPCException {
+    try {
+      return invokeOnce(getInvokers(), rpcRequestWrapper);
+    } catch (RPCException e) {
+      // 如果发生错误，则开启容错模式，显然容错只会对同步调用有效，因为异步调用的返回值为null，无法判断是否发生了异常
+      // 容错处理器
+      FaultToleranceHandler handler = globalConfig.getFaultToleranceHandler();
+      return handler.handle(this, rpcRequestWrapper, e);
+    }
+  }
+
+  private Invoker<?> doSelect(List<Invoker> invokers, RPCRequestWrapper rpcRequestWrapper) {
+    LoadBalancer loadBalancer = globalConfig.getLoadBalancer();
+    RPCRequest request = rpcRequestWrapper.getRpcRequest();
+    while (!invokers.isEmpty()) {
+      Invoker invoker = loadBalancer.select(invokers, request);
+
+      if (!invoker.isAvailable()) {
+        log.warn(
+            "挑选的Invoker不可用：配置[{}]，接口[{}]", invoker.getServiceURL(), invoker.getInterfaceName());
+        invokers.remove(invoker);
+        continue;
+      }
+      return invoker;
+    }
+    // 如果无法查找到可用服务提供者，则抛出异常
+    log.error("无法查到到可用的Invoker");
+    throw new RPCException(ErrorEnum.NO_SERVER_AVAILABLE, "无法查找到可用的服务提供者");
+  }
+
+  /**
+   * 单次调用，如果发生错误，直接抛出（供后续容错处理时调用）
+   *
+   * @param rpcRequestWrapper request
+   * @return RPCResponse
+   */
+  private RPCResponse invokeOnce(List<Invoker> invokers, RPCRequestWrapper rpcRequestWrapper) {
+    // 选择可用的Invoker
+    Invoker<?> invoker = doSelect(invokers, rpcRequestWrapper);
+    RPCThreadPrivateContext.getContext().setInvoker(invoker);
+    RPCResponse response = invoker.invoke(rpcRequestWrapper);
+
+    // response为空，说明是异步调用ASYNC
+    if (response != null && response.hasError()) {
+      Throwable cause = response.getCause();
+
+      // 回收response对象
+      response.recycle();
+      log.error("服务调用发生异常：[{}]", rpcRequestWrapper.getRpcRequest());
+      throw new RPCException(cause, ErrorEnum.SERVICE_INVOCATION_FAILURE, "服务调用失败");
     }
 
-    private void init() {
-        // TODO: ClusterInvoker#init()方法
+    log.info("requestId为[{}]的调用成功", rpcRequestWrapper.getRpcRequest().getRequestId());
+    return response;
+  }
+
+  @Override
+  public Class<T> getInterface() {
+    return interfaceClass;
+  }
+
+  @Override
+  public String getInterfaceName() {
+    return interfaceClass.getName();
+  }
+
+  private List<Invoker> getInvokers() {
+    return new ArrayList<>(invokerMap.values());
+  }
+
+  @Override
+  public ServiceURL getServiceURL() {
+    throw new UnsupportedOperationException("ClusterInvoker不支持getServiceURL()方法");
+  }
+
+  @Override
+  public boolean isAvailable() {
+    for (Invoker<T> invoker : invokerMap.values()) {
+      if (!invoker.isAvailable()) {
+        return false;
+      }
     }
 
-    @Override
-    public RPCResponse invoke(RPCRequestWrapper rpcRequestWrapper) throws RPCException {
-        try {
-            return invokeOnce(getInvokers(), rpcRequestWrapper);
-        } catch (RPCException e) {
-            // 如果发生错误，则开启容错模式，显然容错只会对同步调用有效，因为异步调用的返回值为null，无法判断是否发生了异常
-            // 容错处理器
-            FaultToleranceHandler handler = globalConfig.getFaultToleranceHandler();
-            return handler.handle(this, rpcRequestWrapper, e);
-        }
-    }
+    return true;
+  }
 
-    /**
-     * 增加新的或更新现有的ServiceURL
-     * @param serviceURL ServiceURL
-     */
+  private class EventHandlerAdapter implements EventHandler {
     @SuppressWarnings("unchecked")
-    private synchronized void addOrUpdate(ServiceURL serviceURL) {
-        Protocol protocol = globalConfig.getProtocol();
-        String address = serviceURL.getAddress();
-        String interfaceName = getInterfaceName();
+    @Override
+    public void add(ServiceURL serviceURL) {
+      Protocol protocol = globalConfig.getProtocol();
+      String address = serviceURL.getAddress();
+      String interfaceName = getInterfaceName();
 
-        if (invokerMap.containsKey(address)) {
-            // 如果address服务已经被引用
-            if (protocol instanceof AbstractRemoteProtocol) {
-                // 只有更新远程服务才有意义
-                // 一个Client对应于一个ServiceURL，具体操作由Protocol负责交互
-                log.info("更新服务器配置为[{}]，对应的接口为[{}]", serviceURL, interfaceName);
-                ((AbstractRemoteProtocol) protocol).updateServiceConfig(serviceURL);
-            }
-            return;
-        }
-        // 如果ServiceURL对应的服务尚未被引用
+      if (!invokerMap.containsKey(address)) {
         log.info("添加Invoker，服务器配置为[{}]，对应的接口为[{}]", serviceURL, interfaceName);
         // 引用服务
-        Invoker invoker = protocol.refer(ReferenceConfig.getConfigByInterfaceName(interfaceName), serviceURL);
+        Invoker invoker =
+            protocol.refer(ReferenceConfig.getConfigByInterfaceName(interfaceName), serviceURL);
         // 更新invokerMap
         invokerMap.put(address, (Invoker<T>) invoker);
+      }
     }
 
-    public synchronized void removeNotExisted(List<ServiceURL> serviceURLS) {
-        // TODO: removeNotExisted()方法
-    }
+    @Override
+    public void update(ServiceURL serviceURL) {
+      Protocol protocol = globalConfig.getProtocol();
+      String address = serviceURL.getAddress();
+      String interfaceName = getInterfaceName();
 
-    private Invoker<?> doSelect(List<Invoker> invokers, RPCRequestWrapper rpcRequestWrapper) {
-        LoadBalancer loadBalancer = globalConfig.getLoadBalancer();
-        RPCRequest request = rpcRequestWrapper.getRpcRequest();
-        while (!invokers.isEmpty()) {
-            Invoker invoker = loadBalancer.select(invokers, request);
-
-            if (!invoker.isAvailable()) {
-                log.warn("挑选的Invoker不可用：配置[{}]，接口[{}]", invoker.getServiceURL(), invoker.getInterfaceName());
-                invokers.remove(invoker);
-                continue;
-            }
-            return invoker;
+      if (invokerMap.containsKey(address)) {
+        // 如果address服务已经被引用
+        if (protocol instanceof AbstractRemoteProtocol) {
+          // 只有更新远程服务才有意义
+          // 一个Client对应于一个ServiceURL，具体操作由Protocol负责交互
+          log.info("更新服务器配置为[{}]，对应的接口为[{}]", serviceURL, interfaceName);
+          ((AbstractRemoteProtocol) protocol).updateServiceURL(serviceURL);
         }
-        // 如果无法查找到可用服务提供者，则抛出异常
-        log.error("无法查到到可用的Invoker");
-        throw new RPCException(ErrorEnum.NO_SERVER_AVAILABLE, "无法查找到可用的服务提供者");
-    }
-
-    /**
-     * 单次调用，如果发生错误，直接抛出（供后续容错处理时调用）
-     * @param rpcRequestWrapper request
-     * @return RPCResponse
-     */
-    private RPCResponse invokeOnce(List<Invoker> invokers, RPCRequestWrapper rpcRequestWrapper) {
-        // 选择可用的Invoker
-        Invoker<?> invoker = doSelect(invokers, rpcRequestWrapper);
-        RPCThreadPrivateContext.getContext().setInvoker(invoker);
-        RPCResponse response = invoker.invoke(rpcRequestWrapper);
-
-        // response为空，说明是异步调用ASYNC
-        if (response != null && response.hasError()) {
-            Throwable cause = response.getCause();
-
-            // 回收response对象
-            response.recycle();
-            log.error("服务调用发生异常：[{}]", rpcRequestWrapper.getRpcRequest());
-            throw new RPCException(cause, ErrorEnum.SERVICE_INVOCATION_FAILURE, "服务调用失败");
-        }
-
-        log.info("requestId为[{}]的调用成功", rpcRequestWrapper.getRpcRequest().getRequestId());
-        return response;
+      }
     }
 
     @Override
-    public Class<T> getInterface() {
-        return interfaceClass;
-    }
+    public void remove(ServiceURL serviceURL) {
+      // serviceURL对应的服务器出故障了，应该关闭
+      Protocol protocol = globalConfig.getProtocol();
+      String address = serviceURL.getAddress();
+      String interfaceName = getInterfaceName();
 
-    @Override
-    public String getInterfaceName() {
-        return interfaceClass.getName();
+      invokerMap.remove(address);
+      log.warn("删除Invoker，地址为[{}]，接口名称为[{}]", address, interfaceName);
+      // 关闭address对应的服务器
+      if (protocol instanceof AbstractRemoteProtocol) {
+        ((AbstractRemoteProtocol) protocol).closeClient(address);
+      }
     }
-
-    public List<Invoker> getInvokers() {
-        return new ArrayList<>(invokerMap.values());
-    }
-
-    @Override
-    public ServiceURL getServiceURL() {
-        throw new UnsupportedOperationException("ClusterInvoker不支持getServiceURL()方法");
-    }
-
-    @Override
-    public boolean isAvailable() {
-        for (Invoker<T> invoker : invokerMap.values()) {
-            if (!invoker.isAvailable()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+  }
 }
