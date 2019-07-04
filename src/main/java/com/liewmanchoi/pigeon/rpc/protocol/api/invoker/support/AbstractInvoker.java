6c6aaad6d7be0c6f3780ca9ab45e4ab02a1c4167
Liew Manchoi <liewmanchoi@gmail.com>
2019-07-04 16:53:57 +0800

    style(all): 修改代码风格
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- src/main/java/com/liewmanchoi/pigeon/rpc/protocol/api/invoker/support/AbstractInvoker.java
+++ src/main/java/com/liewmanchoi/pigeon/rpc/protocol/api/invoker/support/AbstractInvoker.java
@@ -7,11 +7,10 @@ import com.liewmanchoi.pigeon.rpc.config.GlobalConfig;
 import com.liewmanchoi.pigeon.rpc.filter.Filter;
 import com.liewmanchoi.pigeon.rpc.protocol.api.invoker.Invoker;
 import com.liewmanchoi.pigeon.rpc.registry.api.ServiceURL;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 
 /**
  * @author wangsheng
@@ -19,50 +18,49 @@ import java.util.List;
  */
 @Slf4j
 public abstract class AbstractInvoker<T> implements Invoker<T> {
-    @Setter
-    private Class<T> interfaceClass;
+  @Setter private Class<T> interfaceClass;
 
-    @Getter
-    @Setter
-    private GlobalConfig globalConfig;
+  @Getter @Setter private GlobalConfig globalConfig;
 
-    @Override
-    public Class<T> getInterface() {
-        return interfaceClass;
+  @Override
+  public Class<T> getInterface() {
+    return interfaceClass;
+  }
+
+  @Override
+  public String getInterfaceName() {
+    return interfaceClass.getName();
+  }
+
+  @Override
+  public ServiceURL getServiceURL() {
+    return ServiceURL.DEFAULT_SERVICE_URL;
+  }
+
+  /**
+   * 构建Filter链
+   *
+   * @param filters Filter对象列表
+   * @return Invoker
+   */
+  public Invoker<T> buildFilterChain(List<Filter> filters) {
+    Invoker<T> last = this;
+
+    if (!filters.isEmpty()) {
+      for (int i = filters.size() - 1; i >= 0; --i) {
+        final Filter filter = filters.get(i);
+        final Invoker<T> next = last;
+
+        last =
+            new AbstractInvokerDelegate<T>(this) {
+              @Override
+              public RPCResponse invoke(RPCRequestWrapper rpcRequestWrapper) throws RPCException {
+                return filter.invoke(next, rpcRequestWrapper);
+              }
+            };
+      }
     }
 
-    @Override
-    public String getInterfaceName() {
-        return interfaceClass.getName();
-    }
-
-    @Override
-    public ServiceURL getServiceURL() {
-        return ServiceURL.DEFAULT_SERVICE_URL;
-    }
-
-    /**
-     * 构建Filter链
-     * @param filters Filter对象列表
-     * @return Invoker
-     */
-    public Invoker<T> buildFilterChain(List<Filter> filters) {
-        Invoker<T> last = this;
-
-        if (!filters.isEmpty()) {
-            for (int i = filters.size() - 1; i >= 0; --i) {
-                final Filter filter = filters.get(i);
-                final Invoker<T> next = last;
-
-                last = new AbstractInvokerDelegate<T>(this) {
-                    @Override
-                    public RPCResponse invoke(RPCRequestWrapper rpcRequestWrapper) throws RPCException {
-                        return filter.invoke(next, rpcRequestWrapper);
-                    }
-                };
-            }
-        }
-
-        return last;
-    }
+    return last;
+  }
 }
