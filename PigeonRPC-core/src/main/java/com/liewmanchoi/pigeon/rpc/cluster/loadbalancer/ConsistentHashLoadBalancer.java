d0d79edc91afe94db0ef8eb07f776ca843424aa4
Liew Manchoi <liewmanchoi@gmail.com>
2019-07-13 09:23:38 +0800

    feat(common.enumeration): LoadBalancerType
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- PigeonRPC-core/src/main/java/com/liewmanchoi/pigeon/rpc/cluster/loadbalancer/ConsistentHashLoadBalancer.java
+++ PigeonRPC-core/src/main/java/com/liewmanchoi/pigeon/rpc/cluster/loadbalancer/ConsistentHashLoadBalancer.java
@@ -1,9 +1,26 @@
 package com.liewmanchoi.pigeon.rpc.cluster.loadbalancer;
 
+import com.liewmanchoi.pigeon.rpc.cluster.ClusterInvoker;
+import com.liewmanchoi.pigeon.rpc.cluster.api.LoadBalancer;
+import com.liewmanchoi.pigeon.rpc.common.domain.RPCRequest;
+import com.liewmanchoi.pigeon.rpc.config.ReferenceConfig;
+import com.liewmanchoi.pigeon.rpc.protocol.api.invoker.Invoker;
+import java.util.List;
+
 /**
  * @author wangsheng
  * @date 2019/7/3
  */
-public class ConsistentHashLoadBalancer {
+public class ConsistentHashLoadBalancer implements LoadBalancer {
+
+  @Override
+  public Invoker<?> select(List<Invoker> invokers, RPCRequest request) {
+    return null;
+  }
+
+  @Override
+  public <T> ClusterInvoker<T> referCluster(ReferenceConfig<T> referenceConfig) {
+    return null;
+  }
   // TODO: 一致性哈希算法的实现
 }
